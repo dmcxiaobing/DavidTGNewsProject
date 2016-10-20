c023c6257e7301d146e616b577fa49bfbe6ca130
qq986945193 <986945193@qq.com>
2016-10-20 17:40:29 +0800

    创建小冰新闻客户端项目

---
+++ app/src/main/java/davidtgnewsproject/qq986945193/com/davidtgnewsproject/constants/Urls.java
@@ -0,0 +1,16 @@
+package davidtgnewsproject.qq986945193.com.davidtgnewsproject.constants;
+
+/**
+ * @author ：程序员小冰
+ * @新浪微博 ：http://weibo.com/mcxiaobing
+ * @GitHub: https://github.com/QQ986945193
+ * @CSDN博客: http://blog.csdn.net/qq_21376985
+ */
+public class Urls {
+    /*取得热点热词分类，可以通过分类id取得热词列表*/
+    public static final String TOP_CLASSIFY = "http://www.tngou.net/api/top/classify";
+    /*取得热点热词列表，也可以用分类id作为参数*/
+    public static final String TOP_LIST = "http://www.tngou.net/api/top/list";
+
+
+}
