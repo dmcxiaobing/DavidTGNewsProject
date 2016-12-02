f43c44444f7feea8abb14a73c9e7b65566e369da
qq986945193 <986945193@qq.com>
2016-12-02 11:32:24 +0800

    更新picasso的封装

--- app/src/main/java/davidtgnewsproject/qq986945193/com/davidtgnewsproject/utils/PicassoWithImageUtils.java
+++ app/src/main/java/davidtgnewsproject/qq986945193/com/davidtgnewsproject/utils/PicassoWithImageUtils.java
@@ -1,9 +1,13 @@
 package davidtgnewsproject.qq986945193.com.davidtgnewsproject.utils;
 
 import android.content.Context;
+import android.graphics.Bitmap;
 import android.widget.ImageView;
 
 import com.squareup.picasso.Picasso;
+import com.squareup.picasso.Transformation;
+
+import davidtgnewsproject.qq986945193.com.davidtgnewsproject.R;
 
 /**
  * @author ：程序员小冰
@@ -26,9 +30,83 @@ public class PicassoWithImageUtils {
      */
     public static void withImageView(Context context, String imageUrl, ImageView imageView) {
         if (imageView != null && context != null && imageUrl != null) {
-            Picasso.with(context).load(imageUrl).into(imageView);
+
+            Picasso.with(context).load(imageUrl)
+                    .fit()
+                    //设置加载之前的默认图片
+                    .placeholder(R.mipmap.ic_launcher)
+                    //设置加载失败的默认图片
+                    .error(R.mipmap.ic_launcher)
+                    .into(imageView);
         }
 
+    }
+
+    /**
+     * Picasso加载图片 将图像进行变换，以更好的适应布局控件等，减小内存开销。
+     *
+     * @param context
+     * @param imageUrl
+     * @param imageView
+     */
+    public static void withImageViewResize(Context context, String imageUrl, ImageView imageView, int width, int height) {
+        if (imageView != null && context != null && imageUrl != null) {
+            Picasso.with(context)
+                    .load(imageUrl)
+                    .fit()
+                    //设置加载之前的默认图片
+                    .placeholder(R.mipmap.ic_launcher)
+                    //设置加载失败的默认图片
+                    .error(R.mipmap.ic_launcher)
+                    .resize(width, height) //200,200
+                    .centerCrop()
+                    .into(imageView);
+        }
+
+    }
+
+    /**
+     * Picasso加载图片 将图像进行变换，实现图像缩小为原来的一半，减小内存开销。
+     * https://github.com/QQ986945193
+     *
+     * @param context
+     * @param imageUrl
+     * @param imageView
+     */
+    public static void withImageViewCustomResize(Context context, String imageUrl, ImageView imageView, int width, int height) {
+        if (imageView != null && context != null && imageUrl != null) {
+            Picasso.with(context).load(imageUrl)
+                    .fit()
+                    //设置加载之前的默认图片
+                    .placeholder(R.mipmap.ic_launcher)
+                    //设置加载失败的默认图片
+                    .error(R.mipmap.ic_launcher)
+                    .transform(new CropSquareTransformation()).into(imageView);
+        }
+
+    }
+
+
+    /**
+     * 自定义接口，实现图像缩小为原来的一半
+     */
+    public static class CropSquareTransformation implements Transformation {
+        @Override
+        public Bitmap transform(Bitmap source) {
+            int size = Math.min(source.getWidth(), source.getHeight());
+            int x = (source.getWidth() - size) / 2;
+            int y = (source.getHeight() - size) / 2;
+            Bitmap result = Bitmap.createBitmap(source, x, y, size, size);
+            if (result != source) {
+                source.recycle();
+            }
+            return result;
+        }
+
+        @Override
+        public String key() {
+            return "square()";
+        }
     }
 
 
